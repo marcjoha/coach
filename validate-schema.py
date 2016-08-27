543dd7dc464ec3a040f025e312f039c864022f74
Marcus Johansson <majohansson@gmail.com>
2016-08-27 22:00:08 +0100

    schema complliant with SL5x5

--- validate-schema.py
+++ validate-schema.py
@@ -1,14 +1,26 @@
 import yaml
-from voluptuous import Schema, Required, Any, Match
+from voluptuous import Schema, Required, Any, Match, Optional
+
+ValidSets = Any(int, Match(r'^(\d+(, \d+)*)$'))
+ValidUnit = Any("kgs", "lbs", "seconds")
 
 schema = Schema({
   Required("name"): str,
   Required("defaults"): {
-    Required("sets"): Any(int, Match(r'^(\d+(, \d+)*)$')),
+    Required("sets"): ValidSets,
     Required("incr"): str,
-    Required("unit"): Any("kgs", "lbs")
-  }
-}, extra=True)
+    Required("unit"): ValidUnit
+  },
+  Required("routine"): [{
+    Required("name"): str,
+    Required("exercises"): [{
+      Required("name"): str,
+      Optional("sets"): ValidSets,
+      Optional("incr"): str,
+      Optional("unit"): ValidUnit
+    }]
+  }]
+})
 
 program = yaml.load(open('program-marcus-5x5.yaml', 'r'))
 
