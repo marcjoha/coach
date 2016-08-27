8627c8d86a858c9d597c029bb66e3fea59bda716
Marcus Johansson <majohansson@gmail.com>
2016-08-27 21:53:47 +0100

    no need to repeat Schema

--- validate-schema.py
+++ validate-schema.py
@@ -3,11 +3,11 @@ from voluptuous import Schema, Required, Any, Match
 
 schema = Schema({
   Required("name"): str,
-  Required("defaults"): Schema({
+  Required("defaults"): {
     Required("sets"): Any(int, Match(r'^(\d+(, \d+)*)$')),
     Required("incr"): str,
     Required("unit"): Any("kgs", "lbs")
-  })
+  }
 }, extra=True)
 
 program = yaml.load(open('program-marcus-5x5.yaml', 'r'))
