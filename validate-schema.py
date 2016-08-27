8bab89a705fb4993121d0f0ec6a9f0080625c18d
Marcus Johansson <majohansson@gmail.com>
2016-08-27 21:46:01 +0100

    first attempt with a schema

---
+++ validate-schema.py
@@ -0,0 +1,15 @@
+import yaml
+from voluptuous import Schema, Required, Any, Match
+
+schema = Schema({
+  Required("name"): str,
+  Required("defaults"): Schema({
+    Required("sets"): Any(int, Match(r'^(\d+(, \d+)*)$')),
+    Required("incr"): str,
+    Required("unit"): Any("kgs", "lbs")
+  })
+}, extra=True)
+
+program = yaml.load(open('program-marcus-5x5.yaml', 'r'))
+
+print schema(program)
